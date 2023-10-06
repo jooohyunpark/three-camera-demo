a5b036666e4f2eabb425cef5944f6afcf7368cb4
Joohyun Park <jhp527@nyu.edu>
2023-10-05 22:14:08 -0400

    wip

--- src/perspective/index.js
+++ src/perspective/index.js
@@ -2,6 +2,7 @@ import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls";
 
+//
 const app = document.querySelector("#app");
 
 //
@@ -17,8 +18,9 @@ const scene = new THREE.Scene();
 //
 const aspect = window.innerWidth / window.innerHeight;
 const camera = new THREE.PerspectiveCamera(50, 0.5 * aspect, 1, 10000);
+camera.position.set(500, 500, 2000);
 camera.lookAt(0, 0, 0);
-camera.position.set(0, 500, 2000);
+
 //
 const cameraPerspective = new THREE.PerspectiveCamera(
   50,
@@ -26,19 +28,20 @@ const cameraPerspective = new THREE.PerspectiveCamera(
   1,
   3000
 );
-cameraPerspective.position.set(0, 300, 0);
+cameraPerspective.position.set(0, 500, 0);
 const cameraPerspectiveHelper = new THREE.CameraHelper(cameraPerspective);
 scene.add(cameraPerspective, cameraPerspectiveHelper);
 
-// ambient light
-const ambientLight = new THREE.AmbientLight(0xffffff, 1);
-scene.add(ambientLight);
+//
+const controls = new OrbitControls(cameraPerspective, renderer.domElement);
+controls.enableZoom = true;
+controls.minDistance = 10;
+controls.maxDistance = 1000;
 
-// ground
+//
 const groundGeometry = new THREE.PlaneGeometry(10000, 10000);
-const groundMaterial = new THREE.MeshStandardMaterial({
-  color: "gray",
-  roughness: 0.8,
+const groundMaterial = new THREE.MeshBasicMaterial({
+  color: "navy",
   side: THREE.DoubleSide,
 });
 const groundMesh = new THREE.Mesh(groundGeometry, groundMaterial);
@@ -46,28 +49,18 @@ groundMesh.position.y = -0.01;
 groundMesh.rotation.x = -Math.PI * 0.5;
 scene.add(groundMesh);
 
-// spheres
+//
 const geometry = new THREE.SphereGeometry(5, 128, 128);
-const material = new THREE.MeshPhongMaterial({
-  color: "#0000ff",
-});
+const material = new THREE.MeshNormalMaterial();
 for (let i = 0; i < 30; i++) {
   const mesh = new THREE.Mesh(geometry, material);
   mesh.position.z = -i * 100;
   scene.add(mesh);
 }
-
-// big sphere
 const sphereMesh = new THREE.Mesh(geometry, material);
 sphereMesh.position.y = 100;
 sphereMesh.scale.setScalar(5);
 scene.add(sphereMesh);
-
-//
-const controls = new OrbitControls(cameraPerspective, renderer.domElement);
-controls.enableZoom = true;
-controls.minDistance = 10;
-controls.maxDistance = 1000;
 
 //
 function onWindowResize() {
@@ -91,6 +84,7 @@ function animate() {
   controls.update();
 }
 
+//
 function render() {
   renderer.clear();
 

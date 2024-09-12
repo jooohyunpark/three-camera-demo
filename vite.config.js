1764a7f7466712de9664e41416fb96eba89f9663
Joohyun Park <jhp527@nyu.edu>
2024-09-11 22:00:48 -0400

    build for github page

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,15 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: resolve(__dirname, "src"),
   server: {
     port: 3000,
+    open: true,
   },
+  // https://vitejs.dev/config/shared-options.html#base
+  base: "/threejs-camera-comparison/",
+  root: resolve(__dirname, "src"),
+  publicDir: resolve(__dirname, "public"),
   build: {
-    outDir: "../dist",
+    outDir: resolve(__dirname, "docs"),
   },
 });
